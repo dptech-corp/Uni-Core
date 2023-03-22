53ab66ae47c8dc72e687f18dfa92085d67d731dc
robotcator <problemset@163.com>
2023-03-22 18:09:20 +0800

    fix cuda install

--- .github/workflows/cuda/cu117-Linux.sh
+++ .github/workflows/cuda/cu117-Linux.sh
@@ -9,7 +9,7 @@ sudo dpkg -i cuda-repo-${OS}-11-7-local_11.7.1-515.65.01-1_amd64.deb
 sudo cp /var/cuda-repo-${OS}-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 sudo apt-get -qq update
-sudo apt install cuda-nvcc-11-7 cuda-libraries-dev-11-7
+sudo apt install cuda cuda-nvcc-11-7 cuda-libraries-dev-11-7
 sudo apt clean
 
 rm -f https://developer.download.nvidia.com/compute/cuda/11.7.1/local_installers/cuda-repo-${OS}-11-7-local_11.7.1-515.65.01-1_amd64.deb
--- .github/workflows/cuda/cu118-Linux.sh
+++ .github/workflows/cuda/cu118-Linux.sh
@@ -9,7 +9,7 @@ sudo dpkg -i cuda-repo-${OS}-11-8-local_11.8.0-520.61.05-1_amd64.deb
 sudo cp /var/cuda-repo-${OS}-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 sudo apt-get -qq update
-sudo apt install cuda-nvcc-11-8 cuda-libraries-dev-11-8
+sudo apt install cuda cuda-nvcc-11-8 cuda-libraries-dev-11-8
 sudo apt clean
 
 rm -f https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-${OS}-11-8-local_11.8.0-520.61.05-1_amd64.deb
