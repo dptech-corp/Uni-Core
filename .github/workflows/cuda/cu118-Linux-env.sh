9eb897a3a4fc9ee487c276ef2a0b4ff4432c0af4
nbrosse <31697743+nbrosse@users.noreply.github.com>
2023-06-01 09:00:35 +0200

    Pipeline for releases (#4)
    * release workflow
    
    ---------
    
    Co-authored-by: nicolas.brosse <nicolas.brosse@iktos.com>

--- .github/workflows/cuda/cu118-Linux-env.sh
+++ .github/workflows/cuda/cu118-Linux-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CUDA_HOME=/usr/local/cuda-11.8
-LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-PATH=${CUDA_HOME}/bin:${PATH}
-
 export FORCE_CUDA=1
 export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
+export CUDA_HOME=/usr/local/cuda-11.8
+export PATH=${CUDA_HOME}/bin:/usr/local/nvidia/bin:/usr/local/nvidia/lib64:$PATH
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:/usr/local/nvidia/lib64:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export CUDA_INSTALL_DIR=$CUDA_HOME
